6588baa1591a2bc221cec50980afc02dfd1c976a
VeniJeen <37582634+VeniJeen@users.noreply.github.com>
2022-01-08 02:52:43 +0100

    update

--- tools/crypto_coin_actual.py
+++ tools/crypto_coin_actual.py
@@ -8,7 +8,7 @@ def date_parser_utc_local(x): return datetime.fromtimestamp(x).strftime('%Y-%m-%
 def get_btc_actual_hourly():
     path=r"C:\Users\Ben\Desktop\Diplomatiki\CryptoSent\Datasets\CryptoCoin Actuals\Bitstamp_BTCUSD_1h.csv"
     df=pd.read_csv(path)
-    df['datetime']=pd.to_datetime(df.unix.progress_apply(date_parser_utc_local))
+    df['datetime']=pd.to_datetime(df.unix.apply(date_parser_utc_local))
     df=df.set_index('datetime')
     df['avg_hl']=(df.high+df.low)/2
     df['avg_oc']=(df.open+df.close)/2
